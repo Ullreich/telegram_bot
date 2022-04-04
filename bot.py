4c0de7d055fcd1881b2b3b45a6f1814086f19113
Ferdinand <ferdiullreich@yahoo.com>
2022-04-04 17:39:22 +0200

    removed token. only storing locally now

--- bot.py
+++ bot.py
@@ -1,15 +1,32 @@
 import telegram
+import argparse
+
+# construct parser
+all_args = argparse.ArgumentParser()
+
+# add args
+all_args.add_argument("-f", "--file", required=True)
+args = vars(all_args)
 
 
 #token that can be generated talking with @BotFather on telegram
-my_token = '5238357535:AAFShZRCdaIMttsBMPKw9DpmOdRTDaGL1zU'
+with open("/home/ferdinand/projects/telegram_bot/vbc_clip_bot_token.txt") as f:
+    my_token = f.read().replace("\n", "")
+my_id = 375269856
 
-def send(msg, chat_id, token=my_token):
+def send(msg, chat_id=my_id, token=my_token):
     """
     Send a message to a telegram user or group specified on chatId
     chat_id must be a number!
     """
     bot = telegram.Bot(token=token)
     bot.sendMessage(chat_id=chat_id, text=msg)
+
+def send_doc(file, chat_id=my_id, token=my_token):
+    # tele_file = telegram.Document("bot.py", "hello_test_1234512345")
+    bot = telegram.Bot(token=token)
+    bot.sendDocument(chat_id=chat_id, document=open("bot.py", "rb"))
     
-send("hi", 375269856)
+if __name__=="__main__":    
+
+    send_doc("hi")
